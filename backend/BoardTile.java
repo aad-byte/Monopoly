757326746897fccefec175590c2529a9a4bf2c8f
aad-byte <158637187+aad-byte@users.noreply.github.com>
2025-06-15 01:04:49 -0400

    moved getTile to static method as a part of class

--- backend/BoardTile.java
+++ backend/BoardTile.java
@@ -10,4 +10,19 @@ public class BoardTile {
     public int getPosition(){
         return position;
     }
+
+    //method to retrive board class object reference based on position on board
+
+    public static BoardTile getTile(int position, BoardData board){
+    BoardTile tile = null;
+    for(int i = 0; i < board.tiles.length; i++){
+        for(int j = 0; j < board.tiles[i].length; j++){
+            if(board.tiles[i][j].getPosition() == position){
+                tile = board.tiles[i][j];
+            }
+        }
+    }
+
+    return tile;
+}
 }
