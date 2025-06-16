ebcec483a4d0373cad3a0df85e6e6d7f44f902ad
YK-261 <yuva22varshika@gmail.com>
2025-06-16 17:43:21 -0400

    Added 4th code to incorporate deletion + search

--- Chance.java
+++ Chance.java
@@ -1,79 +1,73 @@
 import java.util.Random;
 public class Chance extends BoardTile{
-	private static String instruction;
 	
 	public Chance(int position) {
 		super(position);
 	}
+	
+	//Actual Constructor
+	//public Chance(int position, int x, int y) {
+    // super(position, x, y);
+	//}
 
-	public static void performAction(int code, Player player) {
+	private static String instruction;
+	
+	public static void performAction(int code, Player player, BoardData board) {
 		Random rand = new Random();
 		
-		//Type #1: Add/Deduct Cash
-		if(code==1) {
+		//Type #1: Add/Deduct Cash 
+		
+		//Checking if code is one or if code is 4 and the player doesn't own any properties
+		if(code==1 || (code==4 && player.getList().owned==0)) {
 			instruction = "";
-			double amount;
-			do {
-				amount=(rand.nextInt(401)-200)/5;
-			}while(Math.abs(amount)>50);
+			double amount; //Double to hold amount to be generated
 			
-			amount= (double) -200 + (amount*5);
-			player.editCash(amount);
+			do { amount=(double)(rand.nextInt(81)*5)-200; //Generating random amount between -200 and +200 that is a multiple of 5.
+			}while(Math.abs(amount)>=50); //Ensuring that the player gains or loses at least $50 
 			
-			if(amount>0) instruction="Gain $"+amount+"!";
+			player.editCash(amount); //Adding/deducting amount from player's cash
+			
+			//Updating instruction
+			if(amount>0) instruction="Gain $"+amount+"!"; 
 			else  instruction="Lose $"+(amount)*-1+"!";
 		}
 		
 		//Type #2:  Send to Jail 
-		else if(code==1002) {
-			Jail.sendJail(player);
-			instruction="GO TO JAIL!";
+		else if(code==2) {
+			Jail.sendJail(player); //Sending player to jail
+			instruction="GO TO JAIL!"; //Updating instruction
 		}
 		
 		//Type #3: Alter Position
-		else{
-			int posToMove=rand.nextInt(21)+5;
-			posToMove=player.location.getPosition()+posToMove;
-			BoardTile tile =getTile(posToMove, Player.playersGameBoard);
-			player.updatePosition(tile, posToMove);
+		else if (code==3){
+			int posToMove=rand.nextInt(5)+5;//Generates a position between 5-9 inclusive
+
+            instruction="Move up "+posToMove+" positions!"; //Updating instruction
+
+			posToMove=(player.location.getPosition()+posToMove)%24; //% 24 allows player to pass GO tile if they're at the end of the board
+			BoardTile tile = getTile(posToMove, board); //Getting the board tile at the new location
+			player.updatePosition(tile, posToMove, player.latestDiceRoll); //Updating position of the player.	
+		}
+		
+		//Type #4: Surrendering property
+		else {
+			int type; //Variable to store type of property the player must surrender
+			int pos; //Variable to hold the position on the list where the property of generated type is found
+			do {
+				type = rand.nextInt(5)+1; //Generates a random # between 1-5 inclusive to represent type of property to surrender
+				pos = player.getList().search(type); //Updating position
+			}while(pos==-1); //Type generation continues until that type of property exists in the player's list
+	
+			Property deleted= player.getList().delete(pos); //Deleting the property at the list 
+			deleted.setOwner(null); //Setting the owner of the deleted property to null
+			deleted.setOwned(false); //Updating ownership status of the deleted property
 			
-			instruction="Move up "+posToMove+" positions!";
+			instruction = "Surrender Property #"+pos+" of your collection!"; //Updating instruction
 		}
 	}
 	
-	public static String displayInstruction() {
-		return instruction;
+    public static String returnInstruction() {
+		return instruction; //Returning the updated instruction to main
 	}
 
-	/*private static BoardTile searchTile(BoardTile[][] arr, int moveTile) {
-		for(int i=0; i<arr.length; i++) {
-			
-		}
-	}*/
-	
-	/*public String displayInstruction() {
-		return instruction;
-	}
-	
-	public int getCode() {
-		return code;
-	}*/
-	
-	/*Chance(String instruction, int code){
-	this.instruction=instruction;
-	this.code=code;
-}
-
-Chance(String instruction, int max, int min){
-	this.instruction=instruction;
-	this.code=1002;
-	double amount;
-	Random rand = new Random();
-	do {
-		amount=rand.nextInt(((max-min)/10)+1);
-	}while(amount!=0);
-	
-	amount= (double) min + amount*10;
-}*/
-	
 }
